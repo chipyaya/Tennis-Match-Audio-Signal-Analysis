7be484ebc75d9cec5a5fc9dcfd858b87563243f6
chipyaya <b03902024@ntu.edu.tw>
2020-11-01 01:21:11 -0500

    refine line detection + transform

--- mapping/transform.py
+++ mapping/transform.py
@@ -3,11 +3,6 @@ import numpy as np
 from sklearn.cluster import dbscan
 
 
-# Input: A frame of basketball game
-# Output: 
-# 1. A transformation matrix that maps frame coordinate to tactic board coordinate
-# 2. Warped frame
-
 def find_intersection(o1, p1, o2, p2, frame_size):
     def minus(p1, p2):
         return (p1[0] - p2[0], p1[1] - p2[1])
@@ -27,7 +22,7 @@ def find_intersection(o1, p1, o2, p2, frame_size):
         tf = True
     return [tf, (rx, ry)]
 
-def find_intersections(frame, id, line_pts):
+def find_intersections(frame, idx, line_pts):
     # find intersection points
     points = []
     for i in range(len(line_pts)):
@@ -56,49 +51,41 @@ def find_intersections(frame, id, line_pts):
         total[1] = int(total[1] / count)
         centers.append(total)
         cv2.circle(frame, (total[0], total[1]), 10, (255, 0, 0), -1)
-    cv2.imwrite('houghlines-intersections.jpg', frame)
+    cv2.imwrite(out_dir + '{0:03d}-houghlines-intersections.png'.format(idx), frame)
     return centers
 
-def calc_transform_matrix(frame, id, centers):
-    # TODO
-    print(centers)
-    print(frame.shape)
-    # order centers
-    max_x = 0
-    min_x = frame.shape[1]
-    point0, point2 = -1, -1
-    for i in range(len(centers)):
-        if centers[i][0] > max_x:
-            point0 = i
-            max_x = centers[i][0]
-        if centers[i][0] < min_x:
-            point2 = i
-            min_x = centers[i][0]
-    max_y = 0
-    min_y = frame.shape[0]
-    point1, point3 = -1, -1
-    for i in range(len(centers)):
-        if i == point0 or i == point2:
-            continue
-        if centers[i][1] > max_y:
-            point1 = i
-            max_y = centers[i][1]
-        if centers[i][1] < min_y:
-            point3 = i
-            min_y = centers[i][1]
+def find_upper_right(centers, y):
+    centers_filtered = list(filter(lambda p: p[1] <= y, centers))
+    upper_right = max(centers_filtered, key=lambda p: p[0])
+    return upper_right
 
-    # define court point and calculate the transformation matrix
-    court0, court1, court2, court3 = (190, 0), (190, 328), (0, 328), (0, 0)
-    dst_points = np.array([court0, court1, court2, court3], np.float32)
-    src_points = np.array([centers[point0], centers[point1], centers[point2], centers[point3]], np.float32)
+def find_border_points(centers):
+    lower_right = max(centers, key=lambda p: p[0])
+    lower_left = min(centers, key=lambda p: p[0])
+    upper_left = min(centers, key=lambda p: p[1])
+    delta_y = 10
+    upper_right = find_upper_right(centers, upper_left[1] + delta_y)
+    return [upper_left, upper_right, lower_right, lower_left]
+
+def calc_transform_matrix(frame, idx, centers, board):
+    # find border points
+    border_points = find_border_points(centers)
+    src_points = np.array(border_points, np.float32)
+    # define destination court border points
+    border_x_min, border_x_max = 25, 428
+    border_y_min, border_y_max = 27, 248
+    dst_points = np.array([(border_x_min, border_y_max),
+                           (border_x_min, border_y_min),
+                           (border_x_max, border_y_min),
+                           (border_x_max, border_y_max)], np.float32)
 
     # compute the perspective transform matrix and then apply it
     M = cv2.getPerspectiveTransform(src_points, dst_points)
-    warped = cv2.warpPerspective(frame, M, (500, 470))
-    cv2.imwrite('warped.jpg', warped)
+    warped = cv2.warpPerspective(frame, M, board.shape[::-1][1:])
+    cv2.imwrite(warp_out_dir + '{0:03d}-warped.png'.format(idx), warped)
     return M
 
-def detect_edges(frame):
+def detect_edges(frame, idx):
     # Convert BGR to HSV
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -110,24 +97,26 @@ def detect_edges(frame):
     mask = cv2.inRange(frame_hsv, lower_blue, upper_blue)
 
     # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame, frame, mask=mask)
+    # res = cv2.bitwise_and(frame, frame, mask=mask)
 
-    cv2.imwrite('frame.jpg', frame)
-    cv2.imwrite('mask.jpg', mask)
-    cv2.imwrite('res.jpg', res)
+    cv2.imwrite(out_dir + '{0:03d}-frame.png'.format(idx), frame)
+    cv2.imwrite(out_dir + '{0:03d}-mask.png'.format(idx), mask)
+    # cv2.imwrite(out_dir + '{0:03d}-res.png'.format(idx), res)
 
     # erosion and dilation
-    kernel_size = 3
+    kernel_size = 5
     kernel = np.ones((kernel_size, kernel_size), np.uint8)
-    mask = cv2.erode(mask, kernel, iterations=3)
-    mask = cv2.dilate(mask, kernel, iterations=3)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    kernel_size = 10
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
-    cv2.imwrite('mask-erosion-dilation.jpg', mask)
+    cv2.imwrite(out_dir + '{0:03d}-mask-erosion-dilation.png'.format(idx), mask)
 
     # edge detection
     edges = cv2.Canny(mask, 1000, 1500, apertureSize=3)
 
-    cv2.imwrite('mask-edges.jpg', edges)
+    cv2.imwrite(out_dir + '{0:03d}-mask-edges.png'.format(idx), edges)
 
     # calculate Hough lines
     lines = cv2.HoughLines(edges, 1, np.pi/180, 50)
@@ -149,18 +138,27 @@ def detect_edges(frame):
             y2 = int(y0 - 1000*(a))
             cv2.line(frame,(x1, y1), (x2, y2),(0, 0, 255), 2)
             line_pts.append([(x1, y1), (x2, y2)])
-    cv2.imwrite('houghlines.jpg',frame)
+    cv2.imwrite(out_dir + '{0:03d}-houghlines.png'.format(idx), frame)
     return line_pts
  
 if __name__ == '__main__':
+    out_dir = 'out/'
+    warp_out_dir = 'warped/'
     frame_path_format = '../video2image/us19-images/{0:03d}.png'
-    start, end = 2, 3
+    start, end = 1, 210
+    board = cv2.imread('img/tennis-court-board.jpg')
+
 
     for i in range(start, end):
+        print(i)
         frame_path = frame_path_format.format(i)
         frame = cv2.imread(frame_path)
-        line_pts = detect_edges(frame)
-        if edges == None:
+        line_pts = detect_edges(frame, i)
+        if line_pts == None:
             print(frame_path + ': no edges detected')
-        centers = find_intersections(frame, id, line_pts)
-        # calc_transform_matrix(frame, i, centers)
+            continue
+        centers = find_intersections(frame, i, line_pts)
+        if len(centers) == 0:
+            print(frame_path + ': no intersections detected')
+            continue
+        M = calc_transform_matrix(frame, i, centers, board)
