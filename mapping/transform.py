dfddac8337028be8917c6fcc5f388b02e5609178
chipyaya <b03902024@ntu.edu.tw>
2020-11-01 17:34:07 -0600

    tune mask and other params

--- mapping/transform.py
+++ mapping/transform.py
@@ -16,30 +16,39 @@ def detect_edges(out_path, prefix, frame):
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # define range of blue color in HSV
-    lower_blue = np.array([102, 73, 145], np.uint8)
-    upper_blue = np.array([123, 182, 175], np.uint8)
+    lower_blue = np.array([99, 105, 82], np.uint8)
+    upper_blue = np.array([121, 184, 225], np.uint8)
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(frame_hsv, lower_blue, upper_blue)
 
+    # cv2.imwrite(os.path.join(out_path, '{}-frame.png'.format(prefix)), frame)
+    cv2.imwrite(os.path.join(out_path, '{}-mask.png'.format(prefix)), mask)
+
     # erosion and dilation
-    kernel_size = 5
+    kernel_size = 2
     kernel = np.ones((kernel_size, kernel_size), np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    kernel_size = 10
+    kernel_size = 2
     kernel = np.ones((kernel_size, kernel_size), np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
-    # Bitwise-AND mask and original image
-    # res = cv2.bitwise_and(frame, frame, mask=mask)
-
-    # cv2.imwrite(os.path.join(out_path, '{}-frame.png'.format(prefix)), frame)
-    # cv2.imwrite(os.path.join(out_path, '{}-mask.png'.format(prefix)), mask)
-    # cv2.imwrite(os.path.join(out_path, '{}-res.png'.format(prefix)), res)
-    # cv2.imwrite(os.path.join(out_path, '{}-edges.png'.format(prefix)), edges)
+    # filter out top
+    fil = np.zeros(frame.shape[:2], np.uint8)
+    for i in range(70, frame.shape[0]):
+        for j in range(frame.shape[1]):
+            fil[i, j] = 255
+    mask = cv2.bitwise_and(mask, mask, mask=fil)
 
     # edge detection
     edges = cv2.Canny(mask, 1000, 1500, apertureSize=3)
+
+    # Bitwise-AND mask and original image
+    # res = cv2.bitwise_and(frame, frame, mask=mask)
+    # cv2.imwrite(os.path.join(out_path, '{}-res.png'.format(prefix)), res)
+
+    cv2.imwrite(os.path.join(out_path, '{}-mask-morph.png'.format(prefix)), mask)
+    # cv2.imwrite(os.path.join(out_path, '{}-edges.png'.format(prefix)), edges)
 
     # calculate Hough lines
     lines = cv2.HoughLines(edges, 1, np.pi / 180, 50)
@@ -63,7 +72,7 @@ def detect_edges(out_path, prefix, frame):
             line_pts.append([(x1, y1), (x2, y2)])
 
     # cv2.imwrite(os.path.join(out_path,
-    #     '{}-houghlines.png'.format(prefix)), frame)
+        # '{}-houghlines.png'.format(prefix)), frame)
     return line_pts
  
 def find_intersection(o1, p1, o2, p2, frame_size):
@@ -97,10 +106,12 @@ def find_intersections(out_path, prefix, frame, line_pts):
             if tf == True:
                points.append(p)
                cv2.circle(frame, p, 5, (0, 255, 0), -1)
+    if len(points) == 0:
+        return None
     points = np.array(points)
 
     # cluster points and find centers
-    core, lab = dbscan(points, eps=9, min_samples=6)
+    core, lab = dbscan(points, eps=5, min_samples=3)
     centers = []
     for i in range(np.amax(lab) + 1):
         count = 0
@@ -118,20 +129,24 @@ def find_intersections(out_path, prefix, frame, line_pts):
         '{}-intersections.png'.format(prefix)), frame)
     return centers
 
-def find_upper_right(centers, y):
-    centers_filtered = list(filter(lambda p: p[1] <= y, centers))
+def find_upper(centers):
+    # print('centers:', centers)
+    min_y = np.amin(np.array(centers), axis=0)[1]
+    upper_left = min(centers, key=lambda p: p[1])
+    delta_y = 7
+    centers_filtered = list(filter(lambda p: p[1] <= min_y + delta_y, centers))
+    upper_left = min(centers_filtered, key=lambda p: p[0])
     upper_right = max(centers_filtered, key=lambda p: p[0])
-    return upper_right
+    return upper_left, upper_right
 
 def find_border_points(centers):
     lower_right = max(centers, key=lambda p: p[0])
     lower_left = min(centers, key=lambda p: p[0])
-    upper_left = min(centers, key=lambda p: p[1])
-    delta_y = 10
-    upper_right = find_upper_right(centers, upper_left[1] + delta_y)
+    upper_left, upper_right = find_upper(centers)
     return [upper_left, upper_right, lower_right, lower_left]
 
 def calc_transform_matrix(out_path, prefix, court, frame, border_points):
+    # print('border_points:', border_points)
     # the detected border points are the source
     src_points = np.array(border_points, np.float32)
     # define destination border points on the court court 
@@ -154,9 +169,15 @@ def get_human_position(prefix):
 def get_ball_position(prefix):
     file_name = '{}_ball.txt'.format(prefix)
 
+def transform_and_project(court, M, p, color):
+    p = cv2.perspectiveTransform(M, p)
+    cv2.circle(court, p, 10, color, -1)
+
 def project(out_path, prefix, court, M, human_pos, ball_pos):
-    # cv2.circle(court, p, 5, (0, 0, 0), -1)
-    # cv2.imwrite(os.path.join(out_path, '{}-frame.png'.format(prefix)), frame)
+    # for pos in human_pos:
+    #     transform_and_project(court, M, pos, (0, 0, 0))
+    # transform_and_project(court, M, ball_pos, (255, 0, 0))
+    # cv2.imwrite(os.path.join(out_path, '{}-projected.png'.format(prefix)), court)
     pass
 
 if __name__ == '__main__':
@@ -166,9 +187,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--out_dir', type=str, default='results')
     parser.add_argument(
-        '--warped_dir', type=str, default='warped')
-    parser.add_argument(
-        '--border_dir', type=str, default='border')
+        '--preprocessed_dir', type=str, default='preprocessed')
     parser.add_argument(
         '--projected_dir', type=str, default='projected')
     parser.add_argument(
@@ -176,7 +195,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     court = cv2.imread(args.court_img)
-    for dir_name in os.listdir(args.frame_dir):
+    for i, dir_name in enumerate(os.listdir(args.frame_dir)):
         dir_path = os.path.join(args.frame_dir, dir_name)
         if not os.path.isdir(dir_path):
             continue
@@ -186,22 +205,23 @@ if __name__ == '__main__':
             if not os.path.isfile(file_path) or not file_name.endswith('.png'):
                 continue
             frame = cv2.imread(file_path)
-            border_dir = make_out_dir(args.out_dir, dir_name, args.border_dir)
-            warped_dir = make_out_dir(args.out_dir, dir_name, args.warped_dir)
-            projected_dir = make_out_dir(args.out_dir, dir_name, args.projected_dir)
+            preprocessed_dir = make_out_dir(
+                args.out_dir, dir_name, args.preprocessed_dir)
+            projected_dir = make_out_dir(
+                args.out_dir, dir_name, args.projected_dir)
 
-            line_pts = detect_edges(border_dir, prefix, frame)
+            line_pts = detect_edges(preprocessed_dir, prefix, frame)
             if line_pts == None:
                 print('{}: no edges detected'.format(file_path))
                 continue
             centers = find_intersections(
-                border_dir, prefix, frame, line_pts)
-            if len(centers) == 0:
+                preprocessed_dir, prefix, frame, line_pts)
+            if centers == None or len(centers) == 0:
                 print('{}: no intersection found'.format(file_path))
                 continue
             border_points = find_border_points(centers)
             M = calc_transform_matrix(
-                warped_dir, prefix, court, frame, border_points)
+                preprocessed_dir, prefix, court, frame, border_points)
             human_pos = get_human_position(prefix)
             ball_pos = get_ball_position(prefix)
             project(projected_dir, prefix, court, M, human_pos, ball_pos)
