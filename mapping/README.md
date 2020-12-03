f310c2fc3b96398a1062e8367cb5c047f9d35a0c
tyyen <tyyen@utexas.edu>
2020-12-02 18:00:40 -0600

    remove archive files

--- mapping/README.md
+++
@@ -1,5 +0,0 @@
-# WN-FPJ-UTCS
-## Mapping
-```
-python3 transform.py
-```
--- mapping/tools/padding.py
+++
@@ -1,4 +0,0 @@
-import cv2
-img = cv2.imread('court-plan/tennis-court-plan.jpg')
-replicate = cv2.copyMakeBorder(img,10,10,100,100,cv2.BORDER_REPLICATE)
-cv2.imwrite('court-plan/tennis-court-plan-padded.png', replicate)
