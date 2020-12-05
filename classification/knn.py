651c31528e94a27c08fb7245353a671f27ab6783
chipyaya <b03902024@ntu.edu.tw>
2020-12-05 14:58:04 -0600

    try different feature mode: avg, pure, delta

--- classification/knn.py
+++ classification/knn.py
@@ -2,7 +2,9 @@ import os
 import sys
 import pickle
 import argparse
+import textwrap
 import numpy as np
+from argparse import RawTextHelpFormatter
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
@@ -11,39 +13,47 @@ from clipping.audio2mfcc import AudioDataset
 
 
 def parse_arg():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
     parser.add_argument('--target', type=str, default='dis_flag',
         help="possible targets: player_flag, hand_flag, dis_flag, serve_flag")
+    parser.add_argument('--mode', type=str, default='avg',
+        help=textwrap.dedent('''\
+        pure: use pure mfcc wo taking average;
+        delta: use pure mfcc plus delta features;
+        avg: taking average of mfcc features'''))
     args = parser.parse_args()
     return args
 
-def load_datasets():
-    datasets = pickle.load(open('../cached/datasets.p', 'rb'))
+def load_datasets(mode):
+    datasets = pickle.load(open('../cached/datasets-{}.p'.format(mode), 'rb'))
     print(datasets[0][0]['audio'].shape, datasets[0][0]['player_flag'], 
         datasets[0][0]['hand_flag'] , datasets[0][0]['dis_flag'], 
         datasets[0][0]['serve_flag'])
     return datasets
 
-def get_data(target):
-    datasets = load_datasets()
+def get_data(mode, target):
+    datasets = load_datasets(mode)
     X, y = [], []
     for dataset in datasets:
         for i in range(len(dataset)):
-            X.append(dataset[i]['audio'])
+            X.append(dataset[i]['audio'].ravel())
             y.append(dataset[i][target])
     X = np.array(X)
     y = np.array(y)
-    print(X.shape)
-    print(y.shape)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+    print('X_train:{}, X_test:{}'.format(X_train.shape, X_test.shape))
+    print('y_train:{}, y_test:{}'.format(y_train.shape, y_test.shape))
     return X_train, X_test, y_train, y_test
 
 
 if __name__ == '__main__':
     args = parse_arg()
-    X_train, X_test, y_train, y_test = get_data(args.target)
-    classifier = KNeighborsClassifier(n_neighbors=5)
-    classifier.fit(X_train, y_train)
-    y_pred = classifier.predict(X_test)
-    print(confusion_matrix(y_test, y_pred))
-    print(classification_report(y_test, y_pred))
+    X_train, X_test, y_train, y_test = get_data(args.mode, args.target)
+    k_list = [3];
+    for k in k_list:
+        print('k={}'.format(k))
+        classifier = KNeighborsClassifier(n_neighbors=k)
+        classifier.fit(X_train, y_train)
+        y_pred = classifier.predict(X_test)
+        print(confusion_matrix(y_test, y_pred))
+        print(classification_report(y_test, y_pred))
--- clipping/audio2mfcc.py
+++ clipping/audio2mfcc.py
@@ -1,70 +1,87 @@
 import os
 import pickle
 import librosa
+import argparse
+import textwrap
 import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset
+from argparse import RawTextHelpFormatter
 
 
 class AudioDataset(Dataset):
-    def __init__(self, audio_dir, label_dir, audio_file):
+    def __init__(self, audio_dir, label_dir, audio_file, mode):
         self.audio_dir = audio_dir
         self.label_dir = label_dir
         self.audio_file = audio_file
+        self.mode = mode
         self.load_csv()
 
     def load_csv(self):
-        audio_labels_list = pd.read_csv(self.label_dir+self.audio_file+'.csv', header=None, sep='\t', skiprows=1)
-        #self.audio_labels_list = list(audio_labels_list.iloc[:, 0])
+        audio_labels_list = pd.read_csv(self.label_dir+self.audio_file+'.csv',
+            header=None, sep='\t', skiprows=1)
         self.audio_labels_list = audio_labels_list
-        '''
-        print(self.audio_labels_list)
-        print(self.audio_labels_list[0])
-        print(self.audio_labels_list[0][0])
-        '''
+
     def __len__(self):
         return len(self.audio_labels_list)
 
     def __getitem__(self, idx):
-        audio = extract_features(self.audio_dir+self.audio_file+'.wav', self.audio_labels_list[1][idx], self.audio_labels_list[2][idx])
+        audio = extract_features(self.audio_dir+self.audio_file+'.wav',
+            self.audio_labels_list[1][idx], self.audio_labels_list[2][idx],
+            self.mode)
 
         player_flag = self.audio_labels_list[0][idx]
         hand_flag = self.audio_labels_list[3][idx]
         dis_flag = self.audio_labels_list[4][idx]
         serve_flag = self.audio_labels_list[5][idx]
 
-        #print(self.audio_labels_list[:][idx], audio.shape, label)
         return {"audio": audio, "player_flag": player_flag,
                 "hand_flag": hand_flag, "dis_flag": dis_flag, "serve_flag": serve_flag}
 
-def extract_features(f, start, end):
+def extract_features(f, start, end, mode):
     try:
         start = sum(x * int(t) for x, t in zip([60, 1], start.split(":")))
         end = sum(x * int(t) for x, t in zip([60, 1], end.split(":")))
-        #y, sr = librosa.load(f, offset=start, duration=1)
-        y, sr = librosa.load(f, offset=start, duration=end-start+1)
-        mfcc = librosa.feature.mfcc(y, n_mfcc=13)
-        return np.mean(mfcc, axis=1)
-        '''
-        delta = librosa.feature.delta(mfcc)
-        print(delta.shape)
-        return np.vstack([mfcc, delta])
-        '''
+        if mode == 'avg':
+            y, sr = librosa.load(f, offset=start, duration=end-start+1)
+            mfcc = librosa.feature.mfcc(y, n_mfcc=13)
+            return np.mean(mfcc, axis=1)
+        elif mode == 'pure' or mode == 'delta':
+            d = 2
+            y, sr = librosa.load(f, offset=max(0, end-d), duration=2*d)
+            mfcc = librosa.feature.mfcc(y, n_mfcc=13)
+            if mode == 'delta':
+                delta = librosa.feature.delta(mfcc)
+                return np.vstack([mfcc, delta])
+            else:
+                return mfcc
+
     except Exception as e:
         print(e)
         print(f"{f} failed")
 
+def parse_arg():
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument('--mode', type=str, default='avg',
+        help=textwrap.dedent('''\
+        pure: use pure mfcc wo taking average;
+        delta: use pure mfcc plus delta features;
+        avg: taking average of mfcc features'''))
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
+    args = parse_arg()
     audio_dir = '../data/complete_audio/'
     audio_files = ['berrettini_nadal', 'cilic_nadal', 'federer_dimitrov']
     label_dir = '../data/label/'
 
     datasets = []
     for audio_file in audio_files:
-        dataset = AudioDataset(audio_dir, label_dir, audio_file)
+        dataset = AudioDataset(audio_dir, label_dir, audio_file, args.mode)
         print(audio_file)
         print(dataset[0]['audio'].shape, dataset[0]['player_flag'],
             dataset[0]['hand_flag'], dataset[0]['dis_flag'], dataset[0]['serve_flag'])
         datasets.append(dataset)
-    pickle.dump(datasets, open('../cached/datasets.p', 'wb'))
+
+    pickle.dump(datasets, open('../cached/datasets-{}.p'.format(args.mode), 'wb'))
