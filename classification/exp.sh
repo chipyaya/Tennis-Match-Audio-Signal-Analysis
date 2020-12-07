47aadce58974b276cbdfdb3ffc07458b2eee3d95
chipyaya <b03902024@ntu.edu.tw>
2020-12-06 18:03:51 -0600

    add exp script

---
+++ classification/exp.sh
@@ -0,0 +1,15 @@
+declare -a flags=("player_flag" "hand_flag" "dis_flag" "serve_flag")
+declare -a models=("knn" "nb" "rf" "svm-linear" "ridge")
+declare -a modes=("mfcc" "mfcc-avg" "mfcc-delta" "mfcc-4sec" "mel")
+
+for flag in "${flags[@]}"
+do
+    for model in "${models[@]}"
+    do
+        for mode in "${modes[@]}"
+        do
+            echo "flag:" "$flag" ", model:" "$model" ", mode:" "$mode" 
+            python3 ml.py --mode "$mode" --classifier "$model" --target "$flag"
+        done
+    done
+done
